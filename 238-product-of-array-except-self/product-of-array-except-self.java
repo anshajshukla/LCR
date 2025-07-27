7e0ffafbe789d33c1bee1ea3a567253583416610
Anshaj Shukla <91916994+anshajshukla@users.noreply.github.com>
2025-07-27 13:24:52 +0530

    Time: 3 ms (19.66%) | Memory: 55.9 MB (31.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[] productExceptSelf(int[] a) {
-        int n =a.length;
-        int[] p = new int[n];
-        int[] s = new int[n];
-        p[0]=1;
-        s[n-1]=1;
-        for(int i=1;i<n;i++){
-            p[i]=p[i-1]*a[i-1];
-        }
-        for(int i=n-2;i>-1;i--){
-            s[i]=s[i+1]*a[i+1];
-        }
+        // int n =a.length;
+        // int[] p = new int[n];
+        // int[] s = new int[n];
+        // p[0]=1;
+        // s[n-1]=1;
+        // for(int i=1;i<n;i++){
+        //     p[i]=p[i-1]*a[i-1];
+        // }
+        // for(int i=n-2;i>-1;i--){
+        //     s[i]=s[i+1]*a[i+1];
+        // }
+        // int ans[] = new int[n];
+        // for(int i=0;i<n;i++){
+        //     ans[i]=p[i]*s[i];
+        // }
+        // return ans;    
+        int n = a.length;
         int ans[] = new int[n];
+        Arrays.fill(ans,1);
+        int curr=1;
         for(int i=0;i<n;i++){
-            ans[i]=p[i]*s[i];
+            ans[i]*=curr;
+            curr*=a[i];
+        }
+        curr=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]*=curr;
+            curr*=a[i];
         }
         return ans;
-
-        
         
     }
 }
