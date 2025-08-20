e5ee8f0bc2d44e094bf8b22b0a472f502e8fd249
Anshaj Shukla <91916994+anshajshukla@users.noreply.github.com>
2025-08-20 20:53:40 +0530

    Time: 14 ms (93.86%) | Memory: 42.3 MB (78.55%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,32 @@
 class Solution {
-        public String longestPalindrome(String s) {
-                if (s == null || s.length() == 0) {
-                            return "";
-                                    }
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // When loop breaks, pointers are one step beyond the palindrome
+        return right - left - 1;
+    }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        // declare start and end only for the substring calculations 
+        int start = 0;
+        int end = 0;
+        for(int i =0;i<n;i++){
+            // treat every iteration the center 
+            // Case 1 --> 
+            int len1 = expand(s,i,i);
+            int len2 = expand(s,i,i+1);
 
-                                            int start = 0;
-                                                    int end = 0;
+            int max = Math.max(len1,len2);
 
-                                                            for (int i = 0; i < s.length(); i++) {
-                                                                        int odd = expandAroundCenter(s, i, i);
-                                                                                    int even = expandAroundCenter(s, i, i + 1);
-                                                                                                int max_len = Math.max(odd, even);
+            if(max>(end-start+1)){
+                start = i  - (max-1)/2;
+                end = i + max/2;
+            }
 
-                                                                                                            if (max_len > end - start) {
-                                                                                                                            start = i - (max_len - 1) / 2;
-                                                                                                                                            end = i + max_len / 2;
-                                                                                                                                                        }
-                                                                                                                                                                }
-
-                                                                                                                                                                        return s.substring(start, end + 1);        
-                                                                                                                                                                            }
-
-                                                                                                                                                                                private int expandAroundCenter(String s, int left, int right) {
-                                                                                                                                                                                        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                                                                                                                                                                                                    left--;
-                                                                                                                                                                                                                right++;
-                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                return right - left - 1;
-                                                                                                                                                                                                                                    }    
-                                                                                                                                                                                                                                    }
+        }
+        return s.substring(start,end+1);
+        
+    }
+}
