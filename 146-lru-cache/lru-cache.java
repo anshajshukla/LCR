385714736a671849e686702d74d50b39d5f8635f
Anshaj Shukla <91916994+anshajshukla@users.noreply.github.com>
2025-08-30 04:40:27 +0530

    Time: 49 ms (37.71%) | Memory: 117 MB (21.21%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,37 +1,59 @@
 class LRUCache {
+    class Node{
+        int key,value;
+        Node prev,next;
+        Node(int k,int v) { key = k;value=v;};
+    }
     private int capacity;
-    private ArrayList<Integer> cache; // store keys 
-    private HashMap<Integer,Integer> map ; // for key value 
+    private HashMap<Integer,Node> map;
+    private Node head,tail; // dummy node 
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.cache = new ArrayList<>();
         this.map = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next= tail;
+        tail.prev= head;
+        
+    }
+    // Remove nodee from linkedlist 
+    private void remove(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    // Isertion in the linked list 
+    private void insert(Node node){
+        Node prev = tail.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = tail;
+        tail.prev = node;
     }
     
     public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-
-        cache.remove((Integer) key);
-        cache.add(key);
-
-        return map.get(key);
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insert(node); // move to most recent
+        return node.value;
         
     }
     
     public void put(int key, int value) {
-        if (map.containsKey(key)){
-            map.put(key,value);
-            cache.remove((Integer) key);
-            cache.add(key);
-        } else {
-            if(cache.size() == capacity){
-                int lru = cache.remove(0);
-                map.remove(lru);
-            }
-            cache.add(key);
-            map.put(key,value);
+        if (map.containsKey(key)) {
+            // Update existing
+            Node node = map.get(key);
+            remove(node);
+        } else if (map.size() == capacity) {
+            // Evict least recently used (head.next)
+            Node lru = head.next;
+            remove(lru);
+            map.remove(lru.key);
         }
+        Node node = new Node(key, value);
+        insert(node);
+        map.put(key, node);
     }
 }
 
