511b0a832ba7ee876a50e7516ecf8f03c34e2785
Anshaj Shukla <91916994+anshajshukla@users.noreply.github.com>
2025-09-05 03:39:45 +0530

    Time: 45 ms (59.60%) | Memory: 46.9 MB (56.24%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,28 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum =0,target=0;
-        for(int num : nums) sum+=num; 
-        if (sum%2!=0) return false;
-        else target = sum/2;
-        int [][] dp = new int[n][target+1]; 
-        for (int row[] : dp)
-            Arrays.fill(row, -1);
+        int sum = 0;
+        for(int i:nums) sum+=i;
+        int tar = (int) (sum/2);
+        // If sum is odd, can't partition
+        if (sum % 2 != 0) return false;
+        boolean dp[][] = new boolean[nums.length+1][tar+1];
+        // BASE CASE FORGOT
+        for (int i = 0; i <= nums.length; i++) {
+            dp[i][0] = true;
+        }
 
-        return f(n-1,target,nums,dp);
+        for(int i =1;i<=nums.length;i++){
+            for(int j =1;j<=tar;j++){
+                if(nums[i-1]<=j) dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                else dp[i][j] = dp[i-1][j];
+
+            }
+        }
+        return dp[nums.length][tar];
         
-    }
-    private boolean f(int ind, int target , int [] nums,int [][]dp){
-        if(target==0) return true;
-        if(ind==0) return target==nums[0];
-        if(dp[ind][target]!=-1) return dp[ind][target] == 0 ? false : true;
 
-
-        boolean nottake= f(ind-1,target,nums,dp);
-        boolean take = false;
-        if(nums[ind]<=target) take = f(ind-1,target-nums[ind],nums,dp);
-
-        dp[ind][target] = nottake || take ? 1 : 0;
-        return nottake || take;
+        
     }
 }
